01893752bd63c875bac4a591015a650322693b3d
a-watkin <awatkin.work@gmail.com>
2019-03-18 00:03:37 +0000

    getting display word on the frontend

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function($) {
   $.noConflict();
+
+  fetch("http://127.0.0.1:5000/hangman/guess-word", {})
+    .then(res => res.json())
+    .then(result => {
+      $("#disaply-word").append(result["display_word"].join(" "));
+    });
 });
